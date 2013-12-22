c2c9294cf9643254232668914f3192a60530a46d
Garrett Dothard <garrettdothard@gmail.com>
2013-12-22 09:19:29 -0600

    Added Cereal but not implemented

---
+++ ServerCraft/items/itemCereal.java
@@ -0,0 +1,15 @@
+package kaisercode.servercraft.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class itemCereal extends Item {
+	
+	public itemCereal(int id) {
+		super(id);
+		maxStackSize = 64;
+		setCreativeTab(CreativeTabs.tabMaterials);
+		setUnlocalizedName("itemCereal");
+	}
+	
+}
